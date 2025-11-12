95f93b92491c192ebe58411a2c0a9726c61bb34f
gyosic <gyosic@gractor,com>
2025-11-12 16:40:31 +0900

    feat: eslint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -12,7 +14,29 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "components/ui/**",
+    "hooks/use-mobile.ts",
+    "components/bits/**",
   ]),
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      // prettier 권장 설정 (format 미스매치 시 에러 발생)
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+          singleQuote: false,
+          semi: true,
+          trailingComma: "all",
+          printWidth: 100,
+          tabWidth: 2,
+        },
+      ],
+      "react-hooks/exhaustive-deps": "off",
+    },
+  },
+  prettierConfig,
 ]);
 
 export default eslintConfig;
