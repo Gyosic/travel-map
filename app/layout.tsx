e3ed27ca70354091895db103c3884caba3774958
gyosic <gyosic@gractor,com>
2025-11-19 14:12:10 +0900

    feat: 전체 레이아웃 변경 및 sido geojson 데이터 csr로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
+import { CookieArea } from "@/components/shared/CookieArea";
+import { Navbar } from "@/components/shared/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,17 +27,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning className="h-full">
+      <body className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
         <SessionProvider>
-          <ThemeProvider
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
+          <ThemeProvider defaultTheme="system" enableSystem disableTransitionOnChange>
+            <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+              <main className="flex h-screen min-h-screen w-full max-w-3xl flex-col items-center bg-white sm:items-start sm:overflow-hidden dark:bg-black">
+                <CookieArea>
+                  <Navbar navigationLinks={[]} />
+                </CookieArea>
+                {children}
+              </main>
+            </div>
           </ThemeProvider>
         </SessionProvider>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,5 @@
-import { CookieArea } from "@/components/shared/CookieArea";
-import { Navbar } from "@/components/shared/Navbar";
 import TravelMap from "@/components/shared/TravelMap";
-import { site } from "@/config";
-
-const getSidoGeojson = async () => {
-  const sidoGeojson = await fetch(`${site.baseurl}/api/geojson/sido`);
-  const sidoGeojsonData = await sidoGeojson.json();
-  return sidoGeojsonData;
-};
 
 export default async function Home() {
-  // const sggGeojson = await fetch(`${site.baseurl}/api/geojson/sgg`);
-
-  const sidoGeojsonData = await getSidoGeojson();
-
-  // const sggGeojsonData = await sggGeojson.json();
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex h-screen min-h-screen w-full max-w-3xl flex-col items-center bg-white sm:items-start dark:bg-black">
-        <CookieArea>
-          <Navbar navigationLinks={[]} />
-        </CookieArea>
-        <TravelMap sidoGeojson={sidoGeojsonData} />
-      </main>
-    </div>
-  );
+  return <TravelMap />;
 }
