e3a566f5a2ed0053564f414f9ce0e0bace866d11
gyosic <gyosic@gractor,com>
2025-11-20 11:48:22 +0900

    chore: 안쓰는 코드 제거

--- components/shared/TagInput.tsx
+++ components/shared/TagInput.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 interface TagInputProps {
   value?: string[];
