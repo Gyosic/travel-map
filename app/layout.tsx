2add058093d990d349b90be8f14a14eda682082c
gyosic <gyosic@gractor,com>
2025-11-20 11:46:38 +0900

    feat: Toaster 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/tailwind.css";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { CookieArea } from "@/components/shared/CookieArea";
 import { Navbar } from "@/components/shared/Navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="h-full">
       <body className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
         <SessionProvider>
-          <ThemeProvider defaultTheme="system" enableSystem disableTransitionOnChange>
+          <ThemeProvider
+            attribute="data-theme"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
             <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
               <main className="flex h-screen min-h-screen w-full max-w-3xl flex-col items-center bg-white sm:items-start sm:overflow-hidden dark:bg-black">
                 <CookieArea>
@@ -39,6 +45,7 @@ export default function RootLayout({
                 {children}
               </main>
             </div>
+            <Toaster />
           </ThemeProvider>
         </SessionProvider>
       </body>
