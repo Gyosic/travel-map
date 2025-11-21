0e3a7ed76828ce805924831fc3039d233ff236a3
gyosic <gyosic@gractor,com>
2025-11-21 17:55:53 +0900

    feat: history 삭제 및 수정 기능 추가

--- components/shared/TravelPostCard.tsx
+++ components/shared/TravelPostCard.tsx
@@ -1,9 +1,29 @@
 "use client";
 
-import { Calendar, MapPin, Star } from "lucide-react";
+import { Calendar, MapPin, MoreHorizontalIcon, Pencil, Star, Trash2Icon } from "lucide-react";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { toast } from "sonner";
 import { StarButton } from "@/components/shared/StarButton";
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import type { FileType } from "@/lib/schema/file";
 // import { AnimatedTestimonials } from "@/components/ui/shadcn-io/animated-testimonials";
 import { HistoryType } from "@/lib/schema/history.schema";
 
@@ -21,15 +41,49 @@ const AnimatedTestimonials = dynamic(
 
 interface TravelPostCardProps {
   history: HistoryType;
+  onDelete?: () => void;
 }
 
-export function TravelPostCard({ history }: TravelPostCardProps) {
+export function TravelPostCard({ history, onDelete }: TravelPostCardProps) {
+  const router = useRouter();
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const session = useSession();
+
+  const onDeleteItem = async (id: string) => {
+    if (session.status !== "authenticated") {
+      const histories = JSON.parse(localStorage.getItem("histories") || "[]");
+      const newHistories = histories.filter((history: HistoryType) => history._id !== id);
+      localStorage.setItem("histories", JSON.stringify(newHistories));
+
+      toast.success("삭제에 성공했습니다.");
+    } else {
+      const res = await fetch(`/api/histories/${id}`, {
+        method: "DELETE",
+      });
+
+      if (!res.ok) toast.error(await res.text());
+      else toast.success("삭제에 성공했습니다.");
+    }
+
+    setOpenDeleteDialog(false);
+
+    onDelete?.();
+  };
+
+  const handleUpdate = () => {
+    if (session.status !== "authenticated") {
+      toast.error("로그인이 필요합니다.");
+    } else router.push(`/post/${history._id}`);
+  };
   return (
-    <Card className="overflow-hidden">
+    <Card className="relative overflow-hidden">
       <div className="relative h-64 w-full overflow-hidden">
         <AnimatedTestimonials
           className="h-full w-full bg-background/50 p-0 dark:bg-background-foreground/50"
-          testimonials={history?.images?.map((image) => ({ src: `/api/files/${image}` })) ?? []}
+          testimonials={
+            history?.images?.map((image) => ({ src: `/api/files/${(image as FileType).src}` })) ??
+            []
+          }
           autoplay
           autoplayInterval={5000}
           enableBtn={false}
@@ -56,13 +110,57 @@ export function TravelPostCard({ history }: TravelPostCardProps) {
         </div>
       </CardHeader>
 
-      <CardContent>
+      <CardContent className="relative w-full">
         <p className="text-muted-foreground">{history.content}</p>
       </CardContent>
 
       {/* <CardFooter className="border-t pt-4">
         <span className="text-muted-foreground">작성자: {post.author}</span>
       </CardFooter> */}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="absolute top-0 right-0">
+          <Button variant="ghost" size="icon">
+            <MoreHorizontalIcon className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" side="bottom">
+          <DropdownMenuItem onClick={handleUpdate}>
+            <Pencil className="h-4 w-4" />
+            <span>수정</span>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem onClick={() => setOpenDeleteDialog(true)}>
+            <Trash2Icon className="h-4 w-4" />
+            <span>삭제</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Dialog open={openDeleteDialog} onOpenChange={setOpenDeleteDialog}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader className="flex justify-start text-start">
+            <DialogTitle>삭제</DialogTitle>
+            <DialogDescription>
+              삭제한 데이터는 복구할 수 없습니다. 삭제하시겠습니까?
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <div className="flex justify-end gap-2">
+              <Button type="button" variant="outline" onClick={() => setOpenDeleteDialog(false)}>
+                취소
+              </Button>
+              <Button
+                type="button"
+                variant="destructive"
+                onClick={() => {
+                  onDeleteItem(history._id);
+                }}
+              >
+                확인
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </Card>
   );
 }
