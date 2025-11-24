ca8fb0f6a725f01fdded4999fce34949a2a9642a
gyosic <gyosic@gractor,com>
2025-11-24 16:22:41 +0900

    feat: post 페이지일 경우 addbutton hidden 처리

--- components/shared/AddButton.tsx
+++ components/shared/AddButton.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { Plus } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export function AddButton() {
   const router = useRouter();
+  const pathname = usePathname();
+  const isPostPage = pathname.includes("/post");
+
   return (
     <Button
       type="button"
       size="icon"
-      className="fixed right-5 bottom-25 z-50 rounded-full shadow-lg"
+      className={cn("fixed right-5 bottom-25 z-50 rounded-full shadow-lg", isPostPage && "hidden")}
       onClick={() => router.push("/post")}
     >
       <Plus className="" />
