298ca4203dedd7856e7242af13caf22b41811923
gyosic <gyosic@gractor,com>
2025-12-11 14:18:24 +0900

    feat: redis config 추가

--- config.ts
+++ config.ts
@@ -4,7 +4,6 @@ export const logo = {
   light: process.env.LOGO_LIGHT,
   dark: process.env.LOGO_DARK,
 };
-
 export type LogoType = typeof logo;
 
 export const site = {
@@ -12,7 +11,6 @@ export const site = {
   description: process.env.SITE_DESCRIPTION,
   baseurl: process.env.SITE_BASEURL,
 };
-
 export type SiteType = typeof site;
 
 const psqlUrl = urlParse(process.env.PG_BASEURL as string);
@@ -35,5 +33,17 @@ export const geo = {
   database: geoUrl?.pathname.substring(1),
   ssl: false,
 };
+export type GeoPostgresqlType = typeof geo;
+
+const redisUrl = urlParse(process.env.REDIS_BASEURL as string);
+export const redis = {
+  host: redisUrl?.hostname,
+  port: Number(redisUrl?.port || "6379"),
+  user: decodeURIComponent(redisUrl?.username as string),
+  password: decodeURIComponent(redisUrl?.password as string),
+  database: redisUrl?.pathname.substring(1),
+  ssl: false,
+};
+export type RedisType = typeof redis;
 
 export const isDev = process.env.NODE_ENV !== "production";
